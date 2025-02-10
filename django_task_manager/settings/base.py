3445d9226a1843787e97b7bce15a7d089d2b3321
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2025-02-10 12:36:53 +0100

    connecting PostgreSQL to my project, editing settings into a package

--- django_task_manager/settings/base.py
+++ django_task_manager/settings/base.py
@@ -14,8 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -23,15 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
-
 
 # Application definition
 
@@ -79,18 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_task_manager.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -122,7 +103,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 DATETIME_INPUT_FORMATS = [
     "%Y-%m-%d %H:%M",
     "%d.%m.%Y %H:%M",
@@ -147,4 +127,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
-
---
+++ django_task_manager/settings/prod.py
@@ -0,0 +1,21 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+load_dotenv()
+DEBUG = os.getenv("DJANGO_DEBUG", "False") != "False"
+
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': os.environ.get('POSTGRES_DB'),
+           'USER': os.environ.get('POSTGRES_USER'),
+           'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+           'HOST': os.environ.get('POSTGRES_HOST'),
+           'PORT': '5432',
+       }
+   }
