c24da218f835304df7b6c599145ca621a586823c
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2025-02-10 22:09:34 +0100

    connecting static files to my project

--- django_task_manager/settings/base.py
+++ django_task_manager/settings/base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -112,6 +113,8 @@ DATETIME_INPUT_FORMATS = [
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = "staticfiles/"
 
 ASSETS_ROOT = '/static/assets'
 
